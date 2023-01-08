6e32634cf0d2016fe009a8bc86e8a7add1860662
Berrachdi Mohamed <mohamed_berrachdi@um5.ac.ma>
2023-01-08 22:33:45 +0100

    first version of knowbetter

---
+++ pages/Best-tutorials.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.sidebar.markdown(
+    """
+    Copyright © 2023, Scrumaster Inc.
+    """
+)
---
+++ pages/Coding-projects.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="Cooding projects", page_icon="🚀")
+st.sidebar.markdown(
+    """
+    Copyright © 2023, Scrumaster Inc.
+    """
+)
